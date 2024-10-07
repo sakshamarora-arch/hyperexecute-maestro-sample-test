67d91bfc74da3c27b25f7366228710eb53216f97
Aman Chopra <amanchopra.atg@gmail.com>
2024-10-07 11:46:12 +0530

    updated the ios application

--- maestro-test/setup-script.sh
+++ maestro-test/setup-script.sh
@@ -21,7 +21,7 @@
 # Can update the maestro version as required
 mkdir -p ~/maestro-dir
 cd ~/maestro-dir
-export MAESTRO_VERSION=1.36.0; curl -Ls "https://get.maestro.mobile.dev" | bash
+curl -Ls "https://get.maestro.mobile.dev" | bash
 sleep 5
 export PATH="$PATH":"$HOME/.maestro/bin"
 source /home/ltuser/.bashrc
