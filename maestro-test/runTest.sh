4f94a1fa8882df7064eb0531c170c2ace3b6fbe9
Aman Chopra <amanchopra.atg@gmail.com>
2024-10-07 11:34:09 +0530

    updating the maestro ios test

--- maestro-test/runTest.sh
+++ maestro-test/runTest.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # List the running processes on the machine
 ps -ef
-# list the connected devices
-adb devices
 # check the maestro version
-/home/ltuser/.maestro/bin/maestro -v
+/Users/ltuser/.maestro/bin/maestro -v
 # Running the maestro test this can run several tests as passed in the test discovery
-/home/ltuser/.maestro/bin/maestro test ./maestro-test/$1
-
+/Users/ltuser/.maestro/bin/maestro test ./maestro-test/$1
