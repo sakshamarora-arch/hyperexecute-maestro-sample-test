a8dc7c3f509c2b16a59f4ca7464e8e17f62525a3
Aman Chopra <137067777+amanchopra1905@users.noreply.github.com>
2025-07-15 15:04:52 +0530

    updated emulator and simulator scripts

---
+++ maestro-test/runTest_android_emulator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# List the running processes on the machine
+ps -ef
+# list the connected devices
+adb devices
+# Disable WiFi on the emulator
+adb shell svc wifi disable
+# check the maestro version
+/home/ltuser/.maestro/bin/maestro -v
+# Running the maestro test this can run several tests as passed in the test discovery
+/home/ltuser/.maestro/bin/maestro test $1 --debug-output ./MaestroLogs
