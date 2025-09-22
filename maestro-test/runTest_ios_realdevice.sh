cc0bf05461fd3075ec600da6883d78ef9ed9891b
Saksham Arora <sakshamarora@lambdatest.com>
2025-09-22 20:38:14 +0530

    Updated documentation

---
+++ maestro-test/runTest_ios_realdevice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+env
+java -jar ./maestro-test/maestro-cli-1.41.0-all.jar  test ./$1
--- maestro-test/runTest_rd_iOS.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-env
-java -jar ./maestro-test/maestro-cli-1.41.0-all.jar  test ./$1
- 
-
- 
